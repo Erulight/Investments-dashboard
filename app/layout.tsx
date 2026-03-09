298cceefc30adef0f155b8346e654c7dd73948c0
Erulight <lighteru62@gmail.com>
2026-03-09 17:23:12 +0300

    fix light and dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,7 @@ export default function RootLayout({
             __html: `(() => {
   try {
     const stored = localStorage.getItem('theme')
-    const prefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
-    const shouldDark = stored ? stored === 'dark' : prefersDark
+    const shouldDark = stored ? stored === 'dark' : true
     if (shouldDark) document.documentElement.classList.add('dark')
     else document.documentElement.classList.remove('dark')
   } catch {}
